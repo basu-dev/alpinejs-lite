5c2062e52022e9e56e2a84013afb9f215d454a9a
Basu Dev Adhikari <adbasudev54@gmail.com>
2022-11-20 05:03:15 +0545

    Alpine.data api support added

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const todoComponent = () => ({
+  name: "",
+  incompleteTodos: [],
+  completedTodos: [],
+  init() {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((data) => data.json())
+      .then((data) => {
+        data.length = 4;
+        this.incompleteTodos = data.map((d) => d.title);
+      })
+      .catch();
+  },
+  addTodo() {
+    if (this.name.length < 1) return;
+    this.incompleteTodos = [...this.incompleteTodos, this.name];
+    this.name = "";
+  },
+
+  markDone(index) {
+    if (index == -1) return;
+    setTimeout(() => {
+      let todo = this.incompleteTodos.splice(index, 1);
+      this.incompleteTodos = [...this.incompleteTodos];
+      this.completedTodos = [...this.completedTodos, ...todo];
+    }, 300);
+  },
+  removeTodo(index) {
+    setTimeout(() => {
+      this.completedTodos.splice(index, 1);
+      this.completedTodos = [...this.completedTodos];
+    }, 300);
+  },
+  indices: [1, 3, 5],
+});
+const imageComponent = () => ({
+  imageWidth: "",
+  loadingImage: false,
+  src: "https://picsum.photos/534/225.webp",
+  init() {
+    document.getElementById("change-img").addEventListener("load", () => {
+      this.loadingImage = false;
+    });
+  },
+  changeImage() {
+    let height = Math.floor(Math.random() * (500 - 300 + 1) + 300);
+    let width = Math.floor((19 / 8) * height);
+    this.src = `https://picsum.photos/${width}/${height}.webp`;
+    this.loadingImage = true;
+  },
+});
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("todoComponent", () => todoComponent());
+  Alpine.data("imageComponent", () => imageComponent());
+});
